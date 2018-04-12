072266427acd74441a73801d37ef9f0fdd8f9e1c
LisaHuang <1449700087@qq.com>
2018-04-12 14:47:38 +0800

    refactor Order.java and PriceCaculator.java

--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,35 @@
 package practice3;
 
+import java.math.BigDecimal;
+
 public class PriceCaculator {
+    private final Order order;
+
+    public PriceCaculator(Order order) {
+        this.order = order;
+    }
+
+    public BigDecimal calculate() {
+        BigDecimal subTotal = getTotalPrice().subtract(getDiscounts());
+        BigDecimal taxTotal = subTotal.multiply(order.getTax());
+        BigDecimal grandTotal = subTotal.add(taxTotal);
+
+        return grandTotal;
+    }
+
+    private BigDecimal getDiscounts() {
+        BigDecimal result = new BigDecimal(0);
+        for (BigDecimal discount : order.getDiscounts()) {
+            result = result.add(discount);
+        }
+        return result;
+    }
+
+    private BigDecimal getTotalPrice() {
+        BigDecimal result = new BigDecimal(0);
+        for (OrderLineItem lineItem : order.getOrderLineItemList()) {
+            result = result.add(lineItem.getPrice());
+        }
+        return result;
+    }
 }
